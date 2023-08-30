ec66db7b4b9312e07c2c36f7ce4f11c963292e1d
Dan Bauer <daniel.bauer@aerostar.com>
2023-08-30 00:40:45 -0500

    Link up WoodBadge and dbaues.docs
    - Use submodules as public assets and link from about page

---
+++ src/components/ProjectLink.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import WelcomeItem from "./WelcomeItem.vue"
+import DocumentationIcon from "./icons/IconDocumentation.vue"
+import CommunityIcon from "./icons/IconCommunity.vue"
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>dbaues.docs</template>
+
+    Various
+    <a href="./docs/index.html" target="_blank" rel="noopener">documentation</a>
+    I have created while a developer.
+  </WelcomeItem>
+
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Wood Badge</template>
+
+    As a part of my Wood Badge training, I created a site to be used as a template for recruitment that can be found
+    <a href="./WoodBadge/index.html" target="_blank" rel="noopener">here</a>.
+  </WelcomeItem>
+</template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,9 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <a href="./docs/index.html">dbaues.docs</a>
-  </div>
-</template>
+<script setup lang="ts">
+import ProjectLink from "../components/ProjectLink.vue"
+</script>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<template>
+  <main>
+    <ProjectLink />
+  </main>
+</template>
